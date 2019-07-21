edc6de0ae50decfea8be33ff04cc8fa5d3c2e0f7
Ismail Elmaliki <IsmailElmaliki@IsmailElikisMac.fios-router.home>
2019-07-21 09:11:53 -0400

    Completed Hello World iOS app

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Ismail Elmaliki on 7/20/19.
+//  Copyright © 2019 Ismail Elmaliki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
