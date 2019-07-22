1dfadf4f523fec39b14b405856375a5145e839a9
Ismail Elmaliki <IsmailElmaliki@IsmailElikisMac.fios-router.home>
2019-07-22 06:32:21 -0400

    Created user interface for the I Am Rich app

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ismail Elmaliki on 7/21/19.
+//  Copyright © 2019 Ismail Elmaliki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
