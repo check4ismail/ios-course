e7aec40431c9d58094013f429f0686ecc09a7fbd
Ismail Elmaliki <check4elmaliki@live.com>
2019-07-24 09:10:11 -0400

    Created Dicee app - added random functionality for dice change and integrated that into motion sensors

---
+++ Variables-Constants-Datatypes.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var myAge: Int = 30
+
+myAge = 33
+
+let myName: String = "Ishmail"
+let mySurname: String = "Elmaliki"
+let myAgeInTenYears = myAge + 10
+
+// One way of combining strings
+let myFullNameOne = myName + " " + mySurname
+
+// Second way of combining strings
+let myFullNameTwo = "\(myName) \(mySurname)"
+
+let myDetails = "\(myName), \(myAge)"
+
+let booleanExample: Bool = true
+
+let floatNumer: Float = 1.434343
+
+
+
