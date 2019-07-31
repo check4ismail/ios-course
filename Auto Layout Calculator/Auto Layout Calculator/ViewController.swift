c1462bc6d40bade31533819a3dee80755c212494
Ismail Elmaliki <check4elmaliki@live.com>
2019-07-31 08:35:41 -0400

    Created calculator UI using stack views

---
+++ Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Ismail Elmaliki on 7/30/19.
+//  Copyright © 2019 Ismail Elmaliki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	@IBAction func buttonPressed(_ sender: UIButton) {
+	}
+	
+}
+
