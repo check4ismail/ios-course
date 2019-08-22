f2fb535b1ebc96f956b6504b49bd0de628936737
Ismail Elmaliki <check4elmaliki@live.com>
2019-08-22 08:26:54 -0400

    Initial commit for Todoey

---
+++ Todoey/Todoey/TodoListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Ismail Elmaliki on 8/22/19.
+//  Copyright © 2019 Ismail Elmaliki. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
