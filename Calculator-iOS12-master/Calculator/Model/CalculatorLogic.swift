990e948cffe0eebc2b5058b28ceb18308650ee8c
Ismail Elmaliki <check4elmaliki@live.com>
2019-09-07 13:24:52 -0400

    Completed calculator app

--- Calculator-iOS12-master/Calculator/Model/CalculatorLogic.swift
+++ Calculator-iOS12-master/Calculator/Model/CalculatorLogic.swift
@@ -2,30 +2,70 @@
 //  CalculatorLogic.swift
 //  Calculator
 //
-//  Created by Ismail Elmaliki on 9/7/19.
-//  Copyright © 2019 London App Brewery. All rights reserved.
+//  Created by Ismail Elmaliki on 9/7/19
 //
 
 import Foundation
 
 struct CalculatorLogic {
 	
-	var number: Double
+	private var number: Double?
+	private var intermediateCalculation: (n1: Double, calcMethod: String)?
+	private var numCount: Int = 0
 	
-	init(number: Double) {
+	mutating func setNumber(_ number: Double) {
 		self.number = number
 	}
 	
-	func calculate(symbol: String) -> Double? {
+	// MARK: Calculate actions
+	mutating func calculate(symbol: String) -> Double? {
+		if let n = number {
+			numCount += 1
+			switch symbol {
+			case "+/-":
+				return n * -1
+			case "AC":
+				numCount = 0
+				return 0
+			case "%":
+				return n / 100
+			case "=":
+				// 1 number selected then hitting '=' will display that number
+				let result = checkSecondNum() ? performTwoNumCalculation(n2: n) : n
+				return result
+			default:
+				intermediateCalculation = (n1: n, calcMethod: symbol)
+			}
+		}
+			return nil
+	}
 
-		if symbol == "+/-" {
-			return number * -1
-		} else if symbol == "AC" {
-			return 0
-		} else if symbol == "%" {
-			return number / 100
+	// MARK: performs arithmetic
+	private func performTwoNumCalculation(n2: Double) -> Double? {
+		if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+			switch operation {
+			case "+":
+				return n1 + n2
+			case "-":
+				return n1 - n2
+			case "×":
+				return n1 * n2
+			case "÷":
+				return n1 / n2
+			default:
+				fatalError("The operation passed in does not match any of the cases.")
+			}
 		}
 		
 		return nil
+	}
+	
+	// MARK: Determines if secondNumber is selected
+	mutating private func checkSecondNum() -> Bool {
+		if numCount <= 1 {
+			numCount = 0
+			return false
+		}
+		return true
 	}
 }
---
+++ advanced-swift/touples.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+let touple1 = ("Person", 27)
+print(touple1.1)
+
+let touple2 = (name: "Bob", age: 20)
+print(touple2.name)
+
+var touple3: (name: String, age: Int)
+
+touple3.name = "Tom"
+touple3.age = 18
+touple3 = (name: "Tommy", age: 18)
+
+
