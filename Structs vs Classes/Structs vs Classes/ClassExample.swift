239a792032bca97835c5fd3b2c0baaf6d7b7c0dc
Ismail Elmaliki <check4elmaliki@live.com>
2019-09-07 10:39:27 -0400

    Created structs and classes in structs vs. classes project

---
+++ Structs vs Classes/Structs vs Classes/ClassExample.swift
@@ -0,0 +1,19 @@
+//
+//  ClassExample.swift
+//  Structs vs Classes
+//
+//  Created by Ismail Elmaliki on 9/7/19.
+//  Copyright © 2019 Ismail Elmaliki. All rights reserved.
+//
+
+import Foundation
+
+class ClassHero {
+	var name: String
+	var universe: String
+	
+	init(name: String, universe: String) {
+		self.name = name
+		self.universe = universe
+	}
+}
